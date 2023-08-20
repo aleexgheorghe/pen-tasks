41c7d798ef56815c348d057225889cd65b472658
alexeses <46347629+alexeses@users.noreply.github.com>
2023-08-20 13:55:29 +0200

    feat(database): add users table to the database schema
    feat(auth.controller): implement signup functionality to create a new user in the database
    
    feat(auth.routes): switch to using express-promise-router for better error handling and async/await support

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,34 @@
+import bcrypt from "bcrypt";
+import pool from "../db.js";
+
 export const signin = (req, res) => {
 	res.send("Signing in");
 };
 
-export const signup = (req, res) => {
-	res.send("Signing up");
-};
+export const signup = async (req, res, next) => {
+	try {
+		const { name, email, password } = req.body;
 
+		if (!name || !email || !password) {
+			return res.status(400).json({ message: "All fields are required" });
+		}
+
+		const hashPassword = await bcrypt.hash(password, 10);
+
+		await pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)', [name, email, hashPassword]);
+
+		return res.status(201).json({
+			message: "Signup success",
+		});
+	} catch (error) {
+		if (error.code === '23505') {
+			return res.status(400).json({
+				message: "Email already registered",
+			});
+		}
+		next(error);
+	}
+};
 export const signout = (req, res) => {
 	res.send("Logging out");
 };
