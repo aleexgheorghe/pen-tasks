d5fc56362c4a02bc2913e8cd3618d8342c5de216
alexeses <46347629+alexeses@users.noreply.github.com>
2023-08-21 21:24:40 +0200

    chore(front-end): add autoprefixer and tailwindcss dependencies to package.json
    feat(front-end): create postcss.config.js file to configure tailwindcss and autoprefixer plugins
    
    feat(front-end): add Card component
    
    feat(front-end): add Input component
    
    chore(front-end): update index.css to include tailwindcss directives and set font-family
    
    feat(front-end): update RegisterPage to use Input component for form inputs and add styling

--- front-end/src/pages/RegisterPage.jsx
+++ front-end/src/pages/RegisterPage.jsx
@@ -1,8 +1,16 @@
+import {Input} from "../components/ui/Input";
 function RegisterPage() {
 	return (
 		<div>
-            <h1>Register Page</h1>
-            <p>This is the register page</p>
+            <h1 className="text-2xl font-bold text-yellow-200">Register</h1>
+			<form>
+				<Input placeholder="Enter your username" />
+				<Input type="email" placeholder="Enter your email" />
+				<Input type="password" placeholder="Enter your password" />
+			<button>
+				Register
+			</button>
+			</form>
 		</div>
 	);
 }
