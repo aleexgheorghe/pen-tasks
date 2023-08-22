173536ba55ecdde5c999cd9e442e50d1035ef644
alexeses <46347629+alexeses@users.noreply.github.com>
2023-08-22 23:19:08 +0200

    chore(LoginPage.jsx): format code for better readability feat(LoginPage.jsx): add axios post request to submit login form data to the server for authentication

--- front-end/src/pages/LoginPage.jsx
+++ front-end/src/pages/LoginPage.jsx
@@ -1,13 +1,19 @@
 import { Card, Input, Button, Label } from "../components/ui";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
 function LoginPage() {
-
-	const {register, handleSubmit} = useForm();
+	const { register, handleSubmit } = useForm();
 
 	const onSubmit = handleSubmit(async (data) => {
-		console.log(data);
+		const res = await axios.post(
+			"http://localhost:3000/api/v1/auth/signin",
+			data,
+			{
+				withCredentials: true,
+			}
+		);
 	});
 
 	return (
@@ -17,18 +23,32 @@ function LoginPage() {
 
 				<form onSubmit={onSubmit}>
 					<Label htmlFor="email">Email</Label>
-					<Input label="Email" type="email" { ... register("email", {required: true}) } />
-					
+					<Input
+						label="Email"
+						type="email"
+						{...register("email", { required: true })}
+					/>
+
 					<Label htmlFor="password">Password</Label>
-					<Input label="Password" type="password" { ... register("password", {required: true}) } />
+					<Input
+						label="Password"
+						type="password"
+						{...register("password", { required: true })}
+					/>
 
 					<div className="flex flex-col gap-2 mt-4">
 						<Button type="submit">Login</Button>
-						<p className="text-sm text-gray-400">Don't have an account? <Link to="/register" className="text-blue-400">Register</Link></p>
+						<p className="text-sm text-gray-400">
+							Don't have an account?{" "}
+							<Link to="/register" className="text-blue-400">
+								Register
+							</Link>
+						</p>
 					</div>
 				</form>
 			</Card>
 		</div>
 	);
 }
+
 export default LoginPage;
